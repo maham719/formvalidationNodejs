c3fc7941ff89addd80f6f4e99b280637bfdf5a8a
syeda maham amjad <zohameer96@gmail.com>
2025-12-27 04:21:12 +0500

    update

--- index.js
+++ index.js
@@ -13,21 +13,23 @@ app.post("/register",(req,res)=>{
   const {email,password,username}=req.body
   if (usercredentials.email !== email ) {
     if(emailRegex.test(email)){
+
+ if(password.length < 6){
+     const message="password must be 6 characters long"
+   return  res.render("register",{message:message})
+  }
     usercredentials.push({username,email,password})
     const successmessage="registration successfull login to continue"
-    res.render("loginform",{message:null,successmessage:successmessage})
+    return  res.render("loginform",{message:null,successmessage:successmessage})
     }else{
         const message='invalid email format'
-        res.render("register",{message:message})
+       return res.render("register",{message:message})
     } 
   } else{
     const message = 'email already exists'
-    res.render("register",{message:message})
-  }
-  if(password < 6){
-     const message="password must be 6 characters long"
-  return  res.render("loginform",{message:message})
-  }
+   res.render("register",{message:message})
+  } 
+ 
 })
 
 app.get("/loginpage",(req,res)=>{
@@ -38,17 +40,20 @@ app.post('/login',(req,res)=>{
     console.log(email,password)
     if(!email|| !password){
       const message=`please fill all fields`
-     return res.render('loginform',{message:message})
+     return res.render('loginform',{message:message,successmessage:null})
     }else if(email == usercredentials.email || password == usercredentials.password){
         const message= 'login successful'
-       return res.render('loginform',{message:message})
+       return res.render('loginform',{message:message,successmessage:null})
      }else if (!emailRegex.test(email)) {
    const message="invalid email "
-   return res.render("loginform",{message:message})
+   return res.render("loginform",{message:message,successmessage:null})
   }
   if(password !== usercredentials.password ){
     const message="invalid password"
-  return  res.render("loginform",{message:message})
+  return  res.render("loginform",{message:message,successmessage:null})
+  }else{
+    const successmessage="login sucessfull"
+  return  res.render("loginform",{message:null,successmessage:successmessage})
   }
 })
 app.listen(4000,()=>{
